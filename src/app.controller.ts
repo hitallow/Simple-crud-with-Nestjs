3075d4114930c2a1c2a8d7710961ef2dcbfcfd65
Hitallo William <hitallo@viggosistemas.com.br>
2020-03-31 21:11:18 -0300

    configurando módulos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RepoService } from './modules/repo/services/repo.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly repoService: RepoService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('/messages')
+  async getTotalMessages() {
+    return  `Total de livros ${await this.repoService.messageRepo.count()}`;
+  }
+  @Get('/users')
+  async getTotalUsers(){
+    return this.repoService.userRepo.count()
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import * as ormconfig from './config/orm';
+import { RepoModule } from './modules/repo/repo.module';
+import { UserResolver } from './resolvers/user.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MessageResolver } from './resolvers/message.resolver';
+
+const graphQLImports = [UserResolver, MessageResolver];
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    RepoModule,
+    ...graphQLImports,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
